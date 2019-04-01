4abffb077d103d4db8f23edc93a7301783f5338c
Vladimir Trandafilov <trandafilov.vladimir@gmail.com>
2019-04-02 00:05:44 +0300

    feat(lesson13): added cross-browser support + buildDirectory prop

--- src/test/java/lesson13/BaseGUITest.java
+++ src/test/java/lesson13/BaseGUITest.java
@@ -17,11 +17,15 @@ import org.openqa.selenium.Dimension;
 import org.openqa.selenium.Point;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.events.EventFiringWebDriver;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 
+import utils.Browser;
 import utils.EventHandler;
 import utils.SimpleAPI;
 
@@ -31,65 +35,65 @@ public abstract class BaseGUITest extends SimpleAPI {
 
 	protected WebDriver driver;
 
-	@Override
-	protected WebDriver getDriver() {
-		return driver;
-	}
-
 	@Rule
 	public TestWatcher testWatcher = new TestWatcher() {
 		@Override
 		protected void succeeded(Description description) {
-			LOGGER.info(String
-					.format("Test '%s' - PASSED", descriptionToReadableFormat(description)));
+			LOGGER.info(String.format("Test '%s' - PASSED", descriptionToReadableFormat(description)));
 			super.succeeded(description);
 		}
 
 		@Override
 		protected void failed(Throwable e, Description description) {
-			LOGGER.info(String
-					.format("Test '%s' - FAILED due to: %s",
-							descriptionToReadableFormat(description),
-							e.getMessage()));
+			LOGGER.info(String.format("Test '%s' - FAILED due to: %s", descriptionToReadableFormat(description),
+					e.getMessage()));
 			captureScreenshoot(description.getMethodName());
 			super.failed(e, description);
 		}
 
 		@Override
 		protected void skipped(AssumptionViolatedException e, Description description) {
-			LOGGER.info(String
-					.format("Test '%s' - SKIPPED", descriptionToReadableFormat(description)));
+			LOGGER.info(String.format("Test '%s' - SKIPPED", descriptionToReadableFormat(description)));
 			super.skipped(e, description);
 		}
 
 		@Override
 		protected void starting(Description description) {
-			LOGGER.info(String
-					.format("Test '%s' - is starting...", descriptionToReadableFormat(description)));
-			if (driver == null) {
-				final String seleniumUrl = System.getProperty("selenium.url");
-				if (seleniumUrl != null) {
-					DesiredCapabilities caps = DesiredCapabilities.chrome();
-					try {
-						driver = new RemoteWebDriver(new URL("http://" + seleniumUrl + ":4444/wd/hub"), caps);
-					} catch (MalformedURLException e) {
-						e.printStackTrace();
-					}
-				} else {
-					driver = new ChromeDriver();
-				}
-				if (driver == null){
-					throw new RuntimeException("WebDriver wasn't instantiated.");
-				}
-				EventFiringWebDriver wd = new EventFiringWebDriver(driver);
-				wd.register(new EventHandler());
+			LOGGER.info(String.format("Test '%s' - is starting...", descriptionToReadableFormat(description)));
 
-				driver = wd;
-				LOGGER.debug("WebDriver has been started");
-				driver.manage().timeouts().pageLoadTimeout(15, TimeUnit.SECONDS);
-				driver.manage().window().setPosition(new Point(0,0));
-				driver.manage().window().setSize(new Dimension(1920,1080));
+			final Browser browser = Browser.getCurrentBrowser();
+			final DesiredCapabilities desiredCapabilities = initCaps(browser);
+			final String seleniumUrl = System.getProperty("selenium.url");
+
+			if (seleniumUrl != null) {
+				try {
+					driver = new RemoteWebDriver(new URL("http://" + seleniumUrl + ":4444/wd/hub"), desiredCapabilities);
+				} catch (MalformedURLException e) {
+					throw new RuntimeException("Unable to instantiate remote WebDriver:" + e.getMessage());
+				}
+			} else {
+				switch (browser) {
+				case CHROME:
+					final ChromeOptions chromeOptions = new ChromeOptions();
+					driver = new ChromeDriver(chromeOptions.merge(desiredCapabilities));
+					break;
+				case FIREFOX:
+					final FirefoxOptions firefoxOptions = new FirefoxOptions(desiredCapabilities);
+					driver = new FirefoxDriver(firefoxOptions);
+					break;
+				default:
+					throw new RuntimeException(
+							browser.name() + " initialization is not implemented. Try Chrome or Firefox");
+				}
 			}
+			EventFiringWebDriver wd = new EventFiringWebDriver(driver);
+			wd.register(new EventHandler());
+
+			driver = wd;
+			LOGGER.debug("WebDriver has been started");
+			driver.manage().timeouts().pageLoadTimeout(60, TimeUnit.SECONDS);
+			driver.manage().window().setPosition(new Point(0, 0));
+			driver.manage().window().setSize(new Dimension(1920, 1080));
 			super.starting(description);
 		}
 
@@ -102,7 +106,27 @@ public abstract class BaseGUITest extends SimpleAPI {
 		}
 	};
 
-	private String descriptionToReadableFormat(Description description){
+	private DesiredCapabilities initCaps(Browser browser) {
+		DesiredCapabilities desiredCapabilities;
+		switch (browser) {
+		case CHROME:
+			desiredCapabilities = DesiredCapabilities.chrome();
+			break;
+		case FIREFOX:
+			desiredCapabilities = DesiredCapabilities.firefox();
+			break;
+		default:
+			throw new RuntimeException(browser + " browser is not expected");
+		}
+		return desiredCapabilities;
+	}
+
+	@Override
+	protected WebDriver getDriver() {
+		return driver;
+	}
+
+	private String descriptionToReadableFormat(Description description) {
 		return description.getMethodName().replace("_", " ");
 	}
 
@@ -124,9 +148,7 @@ public abstract class BaseGUITest extends SimpleAPI {
 			}
 		}
 		if (!errors.isEmpty()) {
-			throw new AssertionError(errors
-					.stream()
-					.map(assertionError -> "\n Failed" + assertionError.getMessage())
+			throw new AssertionError(errors.stream().map(assertionError -> "\n Failed" + assertionError.getMessage())
 					.collect(Collectors.toList()).toString());
 		}
 	}
